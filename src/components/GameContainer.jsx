5509a1780b888f87d54a58838a16174be2000d82
mmxwillow <mxwierzba@gmail.com>
2023-08-28 16:04:04 +0200

    Disabled buttons if popup is open

--- src/components/GameContainer.jsx
+++ src/components/GameContainer.jsx
@@ -1,9 +1,9 @@
-export default function GameContainer ({pokemons, handleClick}){
+export default function GameContainer ({pokemons, handleClick, disabled}){
     return (
         <div className="game-container">
             {pokemons.map(element => {
                 return (
-                    <button className="card" onClick={handleClick} key={element.id} data-key={element.id}>
+                    <button className="card" disabled={disabled} onClick={handleClick} key={element.id} data-key={element.id}>
                         <img src="" alt="" />
                         <div className="name">{element.name}</div>
                     </button>
